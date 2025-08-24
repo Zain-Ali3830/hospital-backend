413d98dddfecaef5828165a1845a34346c400b14
Zain Ali <zainaliworkmail3830@gmail.com>
2025-08-24 06:21:08 -0700

    interated the API's of conatct and appointment forms

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import router from './app/routes/doctors.routes.js';
+import appointmentRouter from './app/routes/appointments.routes.js';
+import emergencyDocotorsRouter from './app/routes/emergencyDoctors.routes.js';
+import contactRouter from './app/routes/contact.routes.js';
+import cors from 'cors'
 const app=express();
+app.use(cors({origin:"http://localhost:3000"}))
 app.use(express.json());
 
 const port=process.env.PORT || 4000;
 
 app.use("/api/doctors",router)
+app.use("/api",appointmentRouter)
+app.use("/api",emergencyDocotorsRouter)
+app.use("/api",contactRouter)
 app.listen(port,()=>console.log(`Server is running on port ${port}`))
